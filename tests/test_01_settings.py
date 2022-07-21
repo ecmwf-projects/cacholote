1978c5ec4209b52232b21e68794ed6e6ce156f8d
Mattia Almansi <m.almansi@bopen.eu>
2022-07-21 19:22:50 +0200

    better test for settings

--- tests/test_01_settings.py
+++ tests/test_01_settings.py
@@ -7,16 +7,18 @@ import callcache
 def test_settings_change() -> None:
     old_cache = callcache.SETTINGS["cache"]
     new_cache = Cache()
+    assert old_cache is not new_cache
+
     callcache.config(cache=new_cache)
-    assert callcache.SETTINGS["cache"] == new_cache
+    assert callcache.SETTINGS["cache"] is new_cache
 
     # Restore default
     callcache.config(cache=old_cache)
-    assert callcache.SETTINGS["cache"] != new_cache
+    assert callcache.SETTINGS["cache"] is old_cache
 
     with callcache.config(cache=new_cache):
-        assert callcache.SETTINGS["cache"] == new_cache
-    assert callcache.SETTINGS["cache"] != new_cache
+        assert callcache.SETTINGS["cache"] is new_cache
+    assert callcache.SETTINGS["cache"] is old_cache
 
     with pytest.raises(
         ValueError, match="The following settings do NOT exist: {'dummy'}"
