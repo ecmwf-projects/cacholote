2621cc13e87f15551d545facd0230b60d419b940
Mattia Almansi <m.almansi@bopen.eu>
2022-08-10 21:25:33 +0200

    use pickle

--- cacholote/extra_encoders.py
+++ cacholote/extra_encoders.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict
+import pickle
+from typing import Any, Dict, Union
 
 from . import encode
 
@@ -10,19 +11,13 @@ except ImportError:
     HAS_XARRAY = False
 
 
-def dictify_xr_dataarray(obj: "xr.DataArray") -> Dict[str, Any]:
-    return encode.dictify_python_call(
-        "xarray:DataArray.from_dict", obj.to_dict(data=True, encoding=True)
-    )
-
-
-def dictify_xr_dataset(obj: "xr.Dataset") -> Dict[str, Any]:
-    return encode.dictify_python_call(
-        "xarray:Dataset.from_dict", obj.to_dict(data=True, encoding=True)
-    )
+def dictify_xr_object(obj: Union["xr.DataArray", "xr.Dataset"]) -> Dict[str, Any]:
+    # .compute() beacause when pickling an object opened from a NetCDF file,
+    # the pickle file will contain a reference to the file on disk.
+    return encode.dictify_python_call(pickle.loads, pickle.dumps(obj.compute()))
 
 
 def register_all() -> None:
     if HAS_XARRAY:
-        encode.FILECACHE_ENCODERS.append((xr.DataArray, dictify_xr_dataarray))
-        encode.FILECACHE_ENCODERS.append((xr.Dataset, dictify_xr_dataset))
+        encode.FILECACHE_ENCODERS.append((xr.DataArray, dictify_xr_object))
+        encode.FILECACHE_ENCODERS.append((xr.Dataset, dictify_xr_object))
