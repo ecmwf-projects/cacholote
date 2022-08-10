1dd43ba8b807afc1927c0aa1b7cb3a7255dc48a9
Mattia Almansi <m.almansi@bopen.eu>
2022-08-10 13:47:25 +0200

    store xarray object as dict

--- cacholote/extra_encoders.py
+++ cacholote/extra_encoders.py
@@ -1,84 +1,24 @@
-# Copyright 2019, B-Open Solutions srl.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from typing import Any, Dict
 
-
-import pathlib
-from typing import Any, Dict, Union
-
-from . import cache, config, encode
-
-try:
-    import s3fs
-
-    S3 = s3fs.S3FileSystem()
-except ImportError:
-    pass
+from . import encode
 
 try:
     import xarray as xr
+
+    HAS_XARRAY = True
 except ImportError:
-    pass
-
-try:
-    import dask
-except ImportError:
-    pass
+    HAS_XARRAY = False
 
 
-def open_zarr(s3_path: str, *args: Any, **kwargs: Any) -> "xr.Dataset":
-    store = s3fs.S3Map(root=s3_path, s3=S3, check=False)
-    return xr.open_zarr(store=store, *args, **kwargs)  # type: ignore
+def dictify_xr_dataarray(obj: "xr.DataArray") -> Dict[str, Any]:
+    return encode.dictify_python_call("xarray:DataArray.from_dict", obj.to_dict())
 
 
-def tokenize_xr_object(obj: Union["xr.DataArray", "xr.Dataset"]) -> str:
-    with dask.config.set({"tokenize.ensure-deterministic": True}):
-        return str(dask.base.tokenize(obj))  # type: ignore[no-untyped-call]
-
-
-def dictify_xr_dataset_s3(
-    obj: "xr.Dataset",
-    file_name_template: str = "{uuid}.zarr",
-) -> Dict[str, Any]:
-    token = tokenize_xr_object(obj)
-    uuid = cache.hexdigestify(token)
-    file_name = file_name_template.format(**locals())
-    s3_path = f"{config.SETTINGS['directory']}/{file_name}"
-    store = s3fs.S3Map(root=s3_path, s3=S3, check=False)
-    try:
-        xr.open_zarr(store=store)  # type: ignore[no-untyped-call]
-    except:  # noqa: E722
-        obj.to_zarr(store=store)
-    return encode.dictify_python_call(open_zarr, s3_path)
-
-
-def dictify_xr_dataset(
-    obj: Union["xr.DataArray", "xr.Dataset"],
-    file_name_template: str = "{uuid}.nc",
-) -> Dict[str, Any]:
-    token = tokenize_xr_object(obj)
-    uuid = cache.hexdigestify(token)
-    file_name = file_name_template.format(**locals())
-    path = str(pathlib.Path(config.SETTINGS["directory"]).absolute() / file_name)
-    try:
-        xr.open_dataset(path)
-    except:  # noqa: E722
-        obj.to_netcdf(path)
-    return encode.dictify_python_call(xr.open_dataset, path)
+def dictify_xr_dataset(obj: "xr.Dataset") -> Dict[str, Any]:
+    return encode.dictify_python_call("xarray:Dataset.from_dict", obj.to_dict())
 
 
 def register_all() -> None:
-    try:
+    if HAS_XARRAY:
+        encode.FILECACHE_ENCODERS.append((xr.DataArray, dictify_xr_dataarray))
         encode.FILECACHE_ENCODERS.append((xr.Dataset, dictify_xr_dataset))
-    except NameError:
-        pass
--- tests/test_40_extra_encoders.py
+++ tests/test_40_extra_encoders.py
@@ -1,9 +1,8 @@
-import os
-from typing import TypeVar
+from typing import Any, TypeVar
 
 import pytest
 
-from cacholote import cache, config, decode, encode, extra_encoders
+from cacholote import cache, config, decode, encode
 
 xr = pytest.importorskip("xarray")
 T = TypeVar("T")
@@ -13,58 +12,24 @@ def func(a: T) -> T:
     return a
 
 
-def test_dictify_xr_dataset() -> None:
-    data_name = "1dd1448f0d6de747f46e528dc156981434ff6d92dbf1b84383bc5784.nc"
-    data_path = os.path.join(config.SETTINGS["cache_store"].directory, data_name)
-    data = xr.Dataset(data_vars={"data": [0]})
-    expected = {
-        "type": "python_call",
-        "callable": "xarray.backends.api:open_dataset",
-        "args": (data_path,),
-    }
-    res = extra_encoders.dictify_xr_dataset(data)
-    assert res == expected
-    assert os.path.exists(data_path)
-
-    data_name1 = "e7d452a747061ab880887d88814bfb0c27593a73cb7736d2dc340852.nc"
-    data_path1 = os.path.join(config.SETTINGS["cache_store"].directory, data_name1)
-    expected = {
-        "type": "python_call",
-        "callable": "xarray.backends.api:open_dataset",
-        "args": (data_path1,),
-    }
-    data = xr.open_dataset(data_path)
-    res = extra_encoders.dictify_xr_dataset(data)
-    assert res == expected
-    assert os.path.exists(data_path1)
-
-    # skip saving a file already present on disk
-    mtime = os.path.getmtime(data_path1)
-    data = xr.open_dataset(data_path1)
-    res = extra_encoders.dictify_xr_dataset(data)
-    assert res == expected
-    assert mtime == os.path.getmtime(data_path1)
+DATAARRAY = xr.DataArray([0], name="foo")
+XARRAY_OBJECTS = [DATAARRAY, DATAARRAY.to_dataset()]
 
 
-def test_roundtrip() -> None:
-    data = xr.Dataset(data_vars={"data": [0]})
-    date_json = encode.dumps(data)
-    assert decode.loads(date_json).identical(data)
+@pytest.mark.parametrize("obj", XARRAY_OBJECTS)
+def test_roundtrip(obj: Any) -> None:
+    date_json = encode.dumps(obj)
+    assert decode.loads(date_json).identical(obj)
 
 
-def test_cacheable() -> None:
+@pytest.mark.parametrize("obj", XARRAY_OBJECTS)
+def test_cacheable(obj: Any) -> None:
     cfunc = cache.cacheable(func)
 
-    data = xr.Dataset(data_vars={"data": [0]})
-    res = cfunc(data)
-    assert res.identical(data)
+    res = cfunc(obj)
+    assert res.identical(obj)
     assert config.SETTINGS["cache_store"].stats() == (0, 1)
 
-    # FIXME: why do we get two misses?
-    res = cfunc(data)
-    assert res.identical(data)
-    assert config.SETTINGS["cache_store"].stats() == (0, 2)
-
-    res = cfunc(data)
-    assert res.identical(data)
-    assert config.SETTINGS["cache_store"].stats() == (1, 2)
+    res = cfunc(obj)
+    assert res.identical(obj)
+    assert config.SETTINGS["cache_store"].stats() == (1, 1)
