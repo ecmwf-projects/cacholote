f49381e4bde8e2e8e57b86782d37953c9212d57a
Mattia Almansi <m.almansi@bopen.eu>
2022-08-11 18:15:22 +0200

    better testing

--- tests/test_40_extra_encoders.py
+++ tests/test_40_extra_encoders.py
@@ -72,16 +72,18 @@ def test_cacheable() -> None:
 
 def test_copy_file_to_cache_directory(tmpdir: str) -> None:
     tmpfile = os.path.join(tmpdir, "dummy.txt")
+    cached_file = os.path.join(
+        tmpdir, "6b4e03423667dbb73b6e15454f0eb1abd4597f9a1b078e3f5b5a6bc7.txt"
+    )
+
     with open(tmpfile, "w") as f:
         f.write("dummy")
     cfunc = cache.cacheable(func)
 
     res = cfunc(open(tmpfile))
     assert res.read() == "dummy"
-    cached_file = os.path.join(
-        tmpdir, "6b4e03423667dbb73b6e15454f0eb1abd4597f9a1b078e3f5b5a6bc7.txt"
-    )
-    assert os.path.exists(cached_file)
+    with open(cached_file, "r") as f:
+        assert f.read() == "dummy"
     assert config.SETTINGS["cache_store"].stats() == (0, 1)
 
     # skip copying a file already in cache directory
