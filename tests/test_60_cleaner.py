a35e4e0cb2bbe224e51e567f7061de074cd5cfec
Mattia Almansi <m.almansi@bopen.eu>
2022-10-13 18:37:04 +0200

    use bytes

--- tests/test_60_cleaner.py
+++ tests/test_60_cleaner.py
@@ -25,14 +25,14 @@ def test_cleaner(tmpdir: pathlib.Path, set_cache: str, append_info: bool) -> Non
         checksums = []
         for i in range(3):
             time.sleep(1)
-            filename = tmpdir / f"test{i}.txt"
-            with open(filename, "w") as f:
-                f.write("0")
+            filename = tmpdir / f"test{i}"
+            with open(filename, "wb") as f:
+                f.write(b"0")
             checksums.append(fsspec.filesystem("file").checksum(filename))
             open_url(filename)
 
         # Re-use cache file
-        open_url(tmpdir / "test0.txt")
+        open_url(tmpdir / "test0")
 
         assert len(list(utils.cache_store_keys_iter())) == fs.du(dirname) == 3
 
@@ -46,7 +46,7 @@ def test_cleaner(tmpdir: pathlib.Path, set_cache: str, append_info: bool) -> Non
         assert len(list(utils.cache_store_keys_iter())) == fs.du(dirname) == 1
 
         checksum = checksums[0] if append_info else checksums[-1]
-        assert fs.exists(f"{dirname}/{checksum}.txt")
+        assert fs.exists(f"{dirname}/{checksum}")
 
 
 @pytest.mark.parametrize("delete_unknown_files", [True, False])
@@ -58,15 +58,15 @@ def test_clean_unknown(
     fs = utils.get_cache_files_fs()
     dirname = utils.get_cache_files_directory()
 
-    with fs.open(f"{dirname}/unknown.txt", "wt") as f:
-        f.write("0")
+    with fs.open(f"{dirname}/unknown", "wb") as f:
+        f.write(b"0")
 
-    with open(tmpdir / "test.txt", "w") as f:
-        f.write("0")
-    open_url(tmpdir / "test.txt")
+    with open(tmpdir / "test", "wb") as f:
+        f.write(b"0")
+    open_url(tmpdir / "test")
 
     assert fs.du(dirname) == 2
 
     cleaner.cache_files_cleaner(1, delete_unknown_files=delete_unknown_files)
     assert fs.du(dirname) == 1
-    assert fs.exists(f"{dirname}/unknown.txt") is not delete_unknown_files
+    assert fs.exists(f"{dirname}/unknown") is not delete_unknown_files
