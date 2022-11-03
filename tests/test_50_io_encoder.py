de8a53f4350c4f6c5f9d324e366d23a1b6348539
Mattia Almansi <m.almansi@bopen.eu>
2022-11-03 18:42:09 +0100

    tuning

--- tests/test_50_io_encoder.py
+++ tests/test_50_io_encoder.py
@@ -125,7 +125,7 @@ def test_io_corrupted_files(
 def test_io_concurrent_jobs(tmpdir: pathlib.Path, set_cache: bool) -> None:
     # Create file
     tmpfile = tmpdir / "test.txt"
-    fsspec.filesystem("file").pipe_file(tmpfile, b"0" * 10)
+    fsspec.filesystem("file").pipe_file(tmpfile, b"1" * 1_000)
 
     # Cached open
     cfunc = cache.cacheable(open)
@@ -137,7 +137,7 @@ def test_io_concurrent_jobs(tmpdir: pathlib.Path, set_cache: bool) -> None:
         UserWarning, match="can NOT proceed until the cache entry is unlocked"
     ):
         t1.start()
-        time.sleep(0.001)
+        time.sleep(0.005)
         t2.start()
         t1.join()
         t2.join()
