9165ca03edf76348ba811d03652466c2cd37b914
Mattia Almansi <m.almansi@bopen.eu>
2023-03-15 18:09:31 +0100

    fix type check

--- cacholote/config.py
+++ cacholote/config.py
@@ -175,7 +175,7 @@ def reset(env_file: Optional[Union[str, Tuple[str]]] = None) -> None:
         Dot env file(s).
     """
     global _SETTINGS
-    _SETTINGS = Settings(_env_file=env_file)
+    _SETTINGS = Settings(_env_file=env_file)  # type: ignore[call-arg]
     set()
 
 
