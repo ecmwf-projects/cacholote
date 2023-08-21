00d0e77f5a0bc2ceed00bae1dedf9456bbdd175b
Mattia Almansi <m.almansi@bopen.eu>
2023-08-21 16:52:42 +0100

    clear instance cache

--- tests/test_60_clean.py
+++ tests/test_60_clean.py
@@ -227,6 +227,7 @@ def test_clean_invalid_cache_entries(
     )
 
     # Check files
+    fs.clear_instance_cache()
     assert valid in fs.ls(dirname)
     assert (
         corrupted not in fs.ls(dirname) if try_decode else corrupted in fs.ls(dirname)
