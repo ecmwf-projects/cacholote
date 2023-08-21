3ff59a6352aaf4c14a2bf703ce3d952e77c01908
Mattia Almansi <m.almansi@bopen.eu>
2023-08-21 17:07:53 +0100

    restore dirname

--- tests/test_60_clean.py
+++ tests/test_60_clean.py
@@ -227,7 +227,7 @@ def test_clean_invalid_cache_entries(
     )
 
     # Check files
-    fs.invalidate_cache()
+    fs.invalidate_cache(dirname)
     assert valid in fs.ls(dirname)
     assert (
         corrupted not in fs.ls(dirname) if try_decode else corrupted in fs.ls(dirname)
