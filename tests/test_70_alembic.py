c9bb5a611e54ed083f6cf41a20cb1858b95d5fc9
Mattia Almansi <m.almansi@bopen.eu>
2024-07-31 13:27:19 +0200

    qa

--- tests/test_70_alembic.py
+++ tests/test_70_alembic.py
@@ -16,7 +16,6 @@ def old_cache_db_urlpath(tmp_path: pathlib.Path) -> str:
 
 
 def test_old_database(old_cache_db_urlpath: str) -> None:
-    old_db_path = pathlib.Path(__name__) / "data" / "old_cacholote.db"
     cached_time = cache.cacheable(time.time)
     with config.set(cache_db_urlpath=old_cache_db_urlpath):
         actual = cached_time()
